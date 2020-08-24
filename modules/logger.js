4c975f57868d9a3eb5c4935c2d99027b2616da21
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-24 14:22:39 +0200

    feat: logger with morgan and chalk

---
+++ modules/logger.js
@@ -0,0 +1,28 @@
+const morgan = require("morgan");
+const chalk = require("chalk");
+
+/**
+ * Logs the HTTP request into standard out with styling!
+ */
+module.exports.logger = morgan((tokens, req, res) => {
+    const status = tokens.status(req, res);
+    return [
+        chalk.green.bold(tokens.method(req, res)),
+        getStatusEmoji(status),
+        chalk.red.bold(status),
+        chalk.white(tokens.url(req, res)),
+        chalk.yellow(tokens["response-time"](req, res) + " ms"),
+    ].join(" ");
+});
+
+const getStatusEmoji = (code) => {
+    return code >= 500
+        ? "⚠️"
+        : code >= 400
+        ? "❌"
+        : code >= 300
+        ? "🟡"
+        : code >= 200
+        ? "✔️ "
+        : "🟦";
+};
